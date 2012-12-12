6010bd38a5172a7bc0c4caf465e1e08739f4a732
Miguel Campusano <kmpus@Moon-Stone.local>
2012-12-12 16:36:06 -0300

    funciones para obtener el timeline global

--- lib/timeline/helpers.rb
+++ lib/timeline/helpers.rb
@@ -19,5 +19,11 @@ module Timeline
     def get_list(options={})
       Timeline.redis.lrange options[:list_name], options[:start], options[:end]
     end
+
+    def get_global_activity(start = 0, stop = 19)
+      Timeline.redis.lrange("global:activity", start, stop).map do |item|
+          ::Timeline::Activity.new ::Timeline.decode(item)
+      end
+    end
   end
 end
