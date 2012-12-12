3f79710b0c9e964c0ba4b1d6fdc49f97710a28b7
Camilo Palma Pradena <cpalmapradena@gmail.com>
2012-12-12 16:33:21 -0300

    Thread removido por datarace

--- lib/timeline/track.rb
+++ lib/timeline/track.rb
@@ -31,7 +31,7 @@ module Timeline::Track
     private
       def define_activity_method(method_name, options={})
         define_method method_name do
-          puts "TRACK:"+method_name.to_s
+          puts "TRACK: "+method_name.to_s
           @actor = send(options[:actor])
           @fields_for = {}
           @object = set_object(options[:object])
@@ -44,9 +44,12 @@ module Timeline::Track
           @mentionable = options[:mentionable]
           @notificate = options[:notificate]
           @object_double_entry = options[:object_double_entry]
-          Thread.new{
+
+          begin
             add_activity activity(verb: options[:verb])
-          }
+          rescue
+            puts "REDIS ERROR"
+          end
         end
       end
   end
